5c33e10d59b97acde8cac45ae1eb8634fdb4f36e
910980941@qq.com <910980941@qq.com>
2019-04-25 22:53:29 +0800

    返回结果调整

--- src/app/routes/config/icon/icon.component.ts
+++ src/app/routes/config/icon/icon.component.ts
@@ -3,18 +3,16 @@ import { _HttpClient, ModalHelper } from '@delon/theme';
 import { STColumn, STComponent } from '@delon/abc';
 import { SFSchema } from '@delon/form';
 import { ResponseCode } from '@shared/response.code';
+import { NzMessageService } from 'ng-zorro-antd';
+
 @Component({
   selector: 'app-config-icon',
   templateUrl: './icon.component.html',
 })
 export class ConfigIconComponent implements OnInit {
-  data:any={};
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
-
-  ngOnInit() {
-    this.query(null);
-   }
-
+  constructor(private http: _HttpClient, private modal: ModalHelper, private msg: NzMessageService) { }
+  data: any[] = [];
+  users: any[] = [];
   searchSchema: SFSchema = {
     properties: {
       type: {
@@ -37,43 +35,45 @@ export class ConfigIconComponent implements OnInit {
     { title: '类型', index: 'type' },
     { title: '值',  width: '50px', index: 'value' },
     { title: '主题风格',  index: 'theme' },
-    { title: '是否有旋转动画', type:'yn', index: 'spin' },
+    { title: '是否有旋转动画', type: 'yn', index: 'spin' },
     { title: '双色图标的主要颜色', index: 'twoToneColor' },
     { title: 'IconFont 的图标类型',  index: 'iconfont' },
     {
       title: '',
-      // buttons: [
-      //   { text: '查看', click: (item: any) => `/form/${item.id}` },
+      buttons: [
+        { text: '查看', click: (item: any) => `/form/${item.id}` },
       //   { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      // ]
+      ]
     }
   ];
- 
-  query(event:any){
-    let params:any={};
-    if(event){
-      if(event.theme){
-        params.theme_=event.theme;
+
+  ngOnInit() {
+    this.query(null);
+   }
+
+  query(event: any) {
+    const params: any = {};
+    if (event) {
+      if (event.theme) {
+        params.theme_ = event.theme;
       }
-      if(event.type){
-        params.type_=event.type;
+      if (event.type) {
+        params.type_ = event.type;
       }
-      if(event.value){
-        params.value_=event.value;
+      if (event.value) {
+        params.value_ = event.value;
       }
     }
-    this.http.get('api/portal-service/ant-icon/getByParam',params)
+    this.http.get('api/portal-service/ant-icon/getByParam', params)
     .subscribe((res: any) => {
-      if (res && res.code === ResponseCode.SUCCESS){
+      if (res && res.code === ResponseCode.SUCCESS) {
         if (res.data)
           console.log(res);
-          this.data.total=res.data.length;
-          this.data.list=res.data;
-          this.st.data=this.data;
+        this.data = res.data;
       }
      });
   }
-  
+
   add() {
     // this.modal
     //   .createStatic(FormEditComponent, { i: { id: 0 } })
